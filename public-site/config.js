43f5101c72a52c1765ce0c90813163584eb447ac
Wan231 <132431561+Wan231@users.noreply.github.com>
2026-02-26 14:12:32 +0800

    Delete public-site directory

--- public-site/config.js
+++
@@ -1,6 +0,0 @@
-window.PUBLIC_CONFIG = {
-  apiBaseUrl: 'https://kost.wanschool04.workers.dev',
-  listEndpointPath: '/public/listings',
-  bookingEndpointPath: '/public/bookings',
-  fallbackDataPath: './data/sample-listings.json'
-};
